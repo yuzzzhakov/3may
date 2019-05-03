bbdd147022e7eb1fcfff38ec473c34bb307c8019
yuzzzhakov <yuzzzhakaov@ya.ru>
2019-05-03 15:40:25 +0300

    03.05 15.40 - The end of 4

--- blogengine/blog/views.py
+++ blogengine/blog/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+from .models import Post
 
 
 def posts_list(request):
+    posts = Post.objects.all()
+    return render(request, 'blog/index.html', context={'posts': posts})
 
-    return render(request, 'blog/index.html')
+
+def post_detail(request, slug):
+    post = Post.objects.get(slug__iexact=slug)
+    return render(request, 'blog/post_detail.html', context={'post': post})
