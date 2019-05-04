53ec7a4df4b897c25db601e85dacabfb1be3e57d
yuzzzhakov <yuzzzhakaov@ya.ru>
2019-05-04 21:42:06 +0300

    04.05 21.41 8 urok 12 minut

--- blogengine/blog/models.py
+++ blogengine/blog/models.py
@@ -6,6 +6,7 @@ class Post(models.Model):
     title = models.CharField(max_length=150, db_index=True)
     slug = models.SlugField(max_length=150, unique=True)
     body = models.TextField(blank=True, db_index=True)
+    tags = models.ManyToManyField('Tag', blank=True, related_name='posts')
     date_pub = models.DateTimeField(auto_now_add=True)
 
     def get_absolute_url(self):
@@ -13,3 +14,16 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Tag(models.Model):
+    title = models.CharField(max_length=50)
+    slug = models.SlugField(max_length=50, unique=True)
+
+    def get_absolute_url(self):
+        return reverse('tag_detail_url', kwargs={'slug': self.slug})
+
+    def __str__(self):
+        return self.title
+
+
--- blogengine/blog/views.py
+++ blogengine/blog/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
-from .models import Post
+from django.shortcuts import get_object_or_404, render, redirect
+from django.views.generic import View
+from .models import Post, Tag
+from .utils import ObjectDetailMixin
+from .forms import TagForm, PostForm
 
 
 def posts_list(request):
@@ -7,6 +10,51 @@ def posts_list(request):
     return render(request, 'blog/index.html', context={'posts': posts})
 
 
-def post_detail(request, slug):
-    post = Post.objects.get(slug__iexact=slug)
-    return render(request, 'blog/post_detail.html', context={'post': post})
+def tags_list(request):
+    tags = Tag.objects.all()
+    return render(request, 'blog/tags_list.html', context={'tags': tags})
+
+
+class PostDetail(ObjectDetailMixin, View):
+    model = Post
+    template = 'blog/post_detail.html'
+#    def get(self, request, slug):
+#        post = get_object_or_404(Post, slug__iexact=slug)
+#        return render(request, 'blog/post_detail.html', context={'post': post})
+
+
+class TagDetail(ObjectDetailMixin, View):
+    model = Tag
+    template = 'blog/tag_detail.html'
+#    def get(self, request, slug):
+#        tag = get_object_or_404(Tag, slug__iexact=slug)
+#        return render(request, 'blog/tag_detail.html', context={'tag': tag})
+
+
+class TagCreate(View):
+    def get(self, request):
+        form = TagForm()
+        return render(request, 'blog/tag_create.html', context={'form': form})
+
+    def post(self, request):
+        bound_form = TagForm(request.POST)
+
+        if bound_form.is_valid():
+            new_tag = bound_form.save()
+            return redirect(new_tag)
+        return render(request, 'blog/tag_create.html', context={'form': bound_form})
+
+
+class PostCreate(View):
+    def get(self, request):
+        form = PostForm()
+        return render(request, 'blog/post_create_form.html', context={'form': form})
+
+    def post(self, request):
+        bound_form = PostForm(request.POST)
+
+        if bound_form.is_valid():
+            new_post = bound_form.save()
+            return redirect(new_post)
+        return render(request, 'blog/post_create_form.html', context={'form': bound_form})
+
