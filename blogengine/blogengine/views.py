86d98d170e2cb1596756765f9f15f5e9f208dd8d
yuzzzhakov <49301590+yuzzzhakov@users.noreply.github.com>
2019-05-05 17:04:11 +0300

    deleted

--- blogengine/blogengine/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import redirect
-
-
-def redirect_blog(request):
-    return redirect('blog/', permanent=True)
--- envwin/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!d:\ay\mypython\3may\envwin\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
