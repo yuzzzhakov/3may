68f5518b24b8cf76453e6ee996e059ad4e3d19c2
yuzzzhakov <49301590+yuzzzhakov@users.noreply.github.com>
2019-05-07 22:33:36 +0300

    07.05 22.33 Added class Meta for Post and Tag

--- blogengine/blog/models.py
+++ blogengine/blog/models.py
@@ -34,6 +34,9 @@ class Post(models.Model):
     def get_delete_url(self):
         return reverse('post_delete_url', kwargs={'slug': self.slug})
 
+    class Meta:
+        ordering = ['-date_pub']
+
 
 class Tag(models.Model):
     title = models.CharField(max_length=50)
@@ -50,3 +53,8 @@ class Tag(models.Model):
 
     def get_delete_url(self):
         return reverse('tag_delete_url', kwargs={'slug': self.slug})
+
+    class Meta:
+        ordering = ['title']
+
+
