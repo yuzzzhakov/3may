92bb9ec9da871da59b2ec7992eceedf77b9164c7
yuzzzhakov <49301590+yuzzzhakov@users.noreply.github.com>
2019-05-07 23:36:49 +0300

    07.05 23.36 The end of Part 15

--- blogengine/blog/views.py
+++ blogengine/blog/views.py
@@ -6,17 +6,20 @@ from .forms import TagForm, PostForm
 from django.urls import reverse
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.core.paginator import Paginator
+from django.db.models import Q
 
 
 def posts_list(request):
-    posts = Post.objects.all()
+    search_query = request.GET.get('search', '')
+    if search_query:
+        posts = Post.objects.filter(Q(title__icontains=search_query) | Q(body__icontains=search_query))
+    else:
+        posts = Post.objects.all()
+
     paginator = Paginator(posts, 3)
     page_number = request.GET.get('page', 1)
     page = paginator.get_page(page_number)
     is_paginated = page.has_other_pages()
-
-    search_query = request.GET.get('search', '')
-
 
     if page.has_previous():
         prev_url = '?page={}'.format(page.previous_page_number())
