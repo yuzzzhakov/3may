4471ee787c64aa629f8c20ea267c53dc0f494328
yuzzzhakov <49301590+yuzzzhakov@users.noreply.github.com>
2019-05-07 23:28:50 +0300

    07.05 23.28 The end of Part 13

--- blogengine/blog/views.py
+++ blogengine/blog/views.py
@@ -5,11 +5,36 @@ from .utils import *
 from .forms import TagForm, PostForm
 from django.urls import reverse
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.core.paginator import Paginator
 
 
 def posts_list(request):
     posts = Post.objects.all()
-    return render(request, 'blog/index.html', context={'posts': posts})
+    paginator = Paginator(posts, 3)
+    page_number = request.GET.get('page', 1)
+    page = paginator.get_page(page_number)
+    is_paginated = page.has_other_pages()
+
+    search_query = request.GET.get('search', '')
+
+
+    if page.has_previous():
+        prev_url = '?page={}'.format(page.previous_page_number())
+    else:
+        prev_url=''
+
+    if page.has_next():
+        next_url = '?page={}'.format(page.next_page_number())
+    else:
+        next_url=''
+
+    context = {
+        'page_object': page,
+        'is_paginated': is_paginated,
+        'next_url': next_url,
+        'prev_url': prev_url,
+    }
+    return render(request, 'blog/index.html', context=context)
 
 
 def tags_list(request):
